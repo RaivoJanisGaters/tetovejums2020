f98f3342672eac8343c6edc2beb23bb937943798
RaivoJanisGaters <56245897+RaivoJanisGaters@users.noreply.github.com>
2020-11-26 20:54:59 +0200

    Add files via upload

---
+++ errors/forbidden.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Bootstrap Example</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="main.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+    <!-- Brand -->
+    <a class="navbar-brand" href="index.php">
+        <img src="images/nellijalogobalts.png" alt="logo" style="width:90px;">
+    </a>
+
+    <!-- Links -->
+    <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#collapsibleNavbar" aria-expanded="false">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="navbar-collapse collapse" id="collapsibleNavbar" style="">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">Jaunumi</a>
+            </li>
+
+            <!-- Dropdown -->
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="galerija.php" id="navbardrop" data-toggle="dropdown">
+                    Galerija
+                </a>
+                <div class="dropdown-menu">
+                    <a class="dropdown-item" href="galerija.php">Bildes</a>
+                    <a class="dropdown-item" href="video_galerija.php">Video</a>
+                </div>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="kontakti.php">Kontakti</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+            <p style="color: black ; font-size:139px ;" >  403 PIEEJA LIEGTA</p>
+        </div>
+    </div>
+</div>
+<br>
+</body>
+</html>
